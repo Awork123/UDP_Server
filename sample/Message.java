bba2376e3c6912ca3dae603a69c695d489e3479c
Andreas Work <awork@ruc.dk>
2020-04-14 19:47:19 +0200

    used auto generate in Message.java

--- sample/Message.java
+++ sample/Message.java
@@ -4,12 +4,54 @@ public class Message {
     private String message;
     private String IP;
     private int port;
+    private int length;
 
-    public Message(String message, String IP, int port) {
+    public Message(String message, String IP, int port, int length) {
         this.message = message;
         this.IP = IP;
         this.port = port;
+        this.length = length;
+    }
 
+    public String getMessage() {
+        return message;
+    }
 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getIP() {
+        return IP;
+    }
+
+    public void setIP(String IP) {
+        this.IP = IP;
+    }
+//Any reason to get / set port?
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "message='" + message + '\'' +
+                ", IP='" + IP + '\'' +
+                ", port=" + port +
+                ", length=" + length +
+                '}';
     }
 }
